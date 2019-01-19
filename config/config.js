ed6508f80081553724cbc29f5c429ed712c8ef56
Anil Khadka <anil.khadka3777@gmail.com>
2019-01-19 21:02:06 +0545

    process.env.PORT added

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port : 3000,
+    port : process.env.PORT || 3000,
     url : 'mongodb://localhost:27017/RoomFinder',
     mongoOnCloudUrl : "mongodb://nabin19677:wwe19677@roomfindercluster-shard-00-00-q3cpd.mongodb.net:27017,roomfindercluster-shard-00-01-q3cpd.mongodb.net:27017,roomfindercluster-shard-00-02-q3cpd.mongodb.net:27017/test?ssl=true&replicaSet=RoomFinderCluster-shard-0&authSource=admin&retryWrites=true",
     hashingSecret : 'staging process',
