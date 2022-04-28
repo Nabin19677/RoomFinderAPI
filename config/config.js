b0a1bb4edbee7df6c35d74b5c5649bc63d5385c6
Anil Khadka <anil.khadka3777@gmail.com>
2022-04-28 11:59:41 +0545

    change db

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    port : process.env.PORT || 3000,
-    url : 'mongodb://localhost:27017/RoomFinder',
-    mongoOnCloudUrl : "mongodb://nabin19677:wwe19677@roomfindercluster-shard-00-00-q3cpd.mongodb.net:27017,roomfindercluster-shard-00-01-q3cpd.mongodb.net:27017,roomfindercluster-shard-00-02-q3cpd.mongodb.net:27017/test?ssl=true&replicaSet=RoomFinderCluster-shard-0&authSource=admin&retryWrites=true",
-    hashingSecret : 'staging process',
-    jwtSecret : 'It is our jwt secret'
+  port: process.env.PORT || 3000,
+  url: "mongodb://localhost:27017/RoomFinder",
+  mongoOnCloudUrl:
+    "mongodb://nabin19677:wwe19677@roomfindercluster-shard-00-00-q3cpd.mongodb.net:27017/test",
+  // mongoOnCloudUrl : "mongodb://nabin19677:wwe19677@roomfindercluster-shard-00-00-q3cpd.mongodb.net:27017,roomfindercluster-shard-00-01-q3cpd.mongodb.net:27017,roomfindercluster-shard-00-02-q3cpd.mongodb.net:27017/test?ssl=true&replicaSet=RoomFinderCluster-shard-0&authSource=admin&retryWrites=true",
+  hashingSecret: "staging process",
+  jwtSecret: "It is our jwt secret",
 };
